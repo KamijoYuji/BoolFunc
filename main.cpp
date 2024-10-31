867c0cff7207dc4be15b02b496f6a1cb3b37447b
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2024-10-31 13:24:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "boolfunc.h"
 
-int main(){
-    boolfunc a = boolfunc(vector<bool>{1,1,1,1,0,0,1,0});
+int main() {
+    boolfunc a = boolfunc(vector<bool>{0,0,0,1,1,1,1,0});
+    a.ZhegalkinPolynomial();
+    cout << "the Zhegalkin Polynomial: " << a.getExp() << endl;
+    a.DNF();
+    cout << "Perfect DNF: " << a.getExp() << endl;
+    a.KNF();
+    cout << "Perfect KNF: " << a.getExp() << endl;
+    a.BlakeAlg(1);
+    cout << "Blake's algorithm (DNF): " << a.getExp() << endl;
     a.BlakeAlg(0);
+    cout << "Blake's algorithm (KNF): " << a.getExp() << endl;
     return 0;
 }
